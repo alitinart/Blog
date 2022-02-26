2bf47a2079980f1adc368feaf74e967e39fb40e3
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-02-26 17:01:12 +0100

    Main - NOT DONE

--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import Banner from "../../assets/images/Banner@2x.png";
+
 import "./Main.css";
 
 export default function Main() {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <div className="banner">
+        <img src={Banner} />
+      </div>
+      <div className="search-container">
+        <input type={"text"} className="search" placeholder="Search for Post" />
+        <i className="bi bi-search"></i>
+      </div>
+      <div className="divider"></div>
+    </div>
+  );
 }
