3709da2e1174249274d65bf9a6a92cf507ecb36c
Nart Aliti <76398544+alitinart@users.noreply.github.com>
2022-02-26 09:53:10 +0100

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-5xl">A</h1>
+    </div>
+  );
+}
+
+export default App;
